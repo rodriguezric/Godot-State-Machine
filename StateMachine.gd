60d916bf53332eb941f2b0d20adfc6d3a644f16a
= <=>
2020-11-30 20:59:17 -0500

    cleanupg

--- StateMachine.gd
+++ StateMachine.gd
@@ -8,33 +8,33 @@ var states = {}
 onready var parent = get_parent()
 
 func _physics_process(delta):
-    if state != null:
-        _state_logic(delta)
-        var transition = _get_transition(delta)
-        if transition != null:
-            set_state(transition)
+	if state != null:
+		_state_logic(delta)
+		var transition = _get_transition(delta)
+		if transition != null:
+			set_state(transition)
 
 func _state_logic(delta):
-    pass
+	pass
 
 func _get_transition(delta):
-    return null
+	return null
 
 func _enter_state(new_state, old_state):
-    pass
+	pass
 
 func _exit_state(old_state, new_state):
-    pass
+	pass
 
 func set_state(new_state):
-    previous_state = state
-    state = new_state
+	previous_state = state
+	state = new_state
 
-    if previous_state != null:
-        _exit_state(previous_state, new_state)
+	if previous_state != null:
+		_exit_state(previous_state, new_state)
 
-    if new_state != null:
-        _enter_state(new_state, previous_state)
+	if new_state != null:
+		_enter_state(new_state, previous_state)
 
 func add_state(state_name):
-    states[state_name] = states.size()
+	states[state_name] = states.size()
